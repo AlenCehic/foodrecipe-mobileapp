a76ef55ebf99a5d21fc2e85493ff79425991a16e
scehia <alen.cehic_13@hotmail.com>
2025-02-21 14:00:54 +0100

    new commit

--- components/BottomNavIcon.js
+++
@@ -1,6 +0,0 @@
-import Ionicons from "@expo/vector-icons/Ionicons"
-
-export function BottomNavIcon({ name, color, focused }) {
-    const iconName = focused ? name.iconFocused : name.icon
-    return <Ionicons size={28} style={{ marginBottom: -2 }} name={iconName} color={color} />
-}
